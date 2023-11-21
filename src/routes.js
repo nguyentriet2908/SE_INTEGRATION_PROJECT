9df0723d07ae9358d1ed311dc751cd15705ce6cb
quocthanh03 <quocthanh.280603@gmail.com>
2023-11-21 22:20:39 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 import Dashboard from "views/admin/Dashboard.js";
 import Profile from "views/admin/Profile.js";
 import Calendar from "views/admin/Calendar.js";
-import Register from "views/admin/Register.js";
-import Logout from "views/admin/Login.js";
+import Login from "views/admin/Login.js";
 import Tables from "views/admin/Tables.js";
 import Notes from "views/admin/Notes.js";
 import Icons from "views/admin/Icons.js";
@@ -44,25 +43,18 @@ var routes = [
     layout: "/admin",
   },
   {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
-    component: <Logout />,
-    layout: "/auth",
-  },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: <Register />,
-    layout: "/auth",
-  },
-  {
     path: "/icons",
     name: "Icons",
     icon: "ni ni-planet text-blue",
     component: <Icons />,
     layout: "/admin",
+  },
+  {
+    path: "/login",
+    name: "Logout",
+    icon: "ni ni-key-25 text-info",
+    component: <Login />,
+    layout: "/auth",
   },
 ];
 export default routes;
