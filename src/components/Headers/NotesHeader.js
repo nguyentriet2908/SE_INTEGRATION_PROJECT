e1c70c91fde19a22df04a523e6c71e15a38d06bf
Tran Minh Triet <tranminhtriet0@gmail.com>
2023-11-21 23:59:04 +0700

    feat: implement basic notes functionality

---
+++ src/components/Headers/NotesHeader.js
@@ -0,0 +1,14 @@
+// reactstrap components
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+const NotesHeader = () => {
+    return (
+        <>
+          <div className="header bg-gradient-info pb-6 pt-3 pt-md-4">
+          </div>
+        </>
+      );
+    
+};
+
+export default NotesHeader;
